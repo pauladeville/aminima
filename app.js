32a307b494d30937380d217d790437e5fbf39308
P <paula.deville@posteo.net>
2023-03-15 18:12:28 +0100

    structure mobile OK

---
+++ app.js
@@ -0,0 +1,18 @@
+//HAMBURGER
+
+const burger = document.querySelector('.hamburger');
+const menuMobile = document.querySelector('.menu-mobile');
+const menuMobileLinks = document.querySelectorAll('.menu-mobile__item a');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('hamburger--close');
+    menuMobile.classList.toggle('menu-mobile--close');
+});
+for(menuMobileLink of menuMobileLinks){
+  menuMobileLink.addEventListener('click', () => {
+    burger.classList.toggle('hamburger--close');
+    menuMobile.classList.toggle('menu-mobile--close');
+  })
+}
+
+
+
