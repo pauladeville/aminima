bfb9b2c08e562f14fa8f2e5a57aaec79c7ff2629
P <paula.deville@posteo.net>
2023-03-20 16:07:08 +0100

    animation mobile ok

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 //HAMBURGER
 
-const burger = document.querySelector('.hamburger');
-const menuMobile = document.querySelector('.menu-mobile');
-const menuMobileLinks = document.querySelectorAll('.menu-mobile__item a');
-burger.addEventListener('click', () => {
-    burger.classList.toggle('hamburger--close');
-    menuMobile.classList.toggle('menu-mobile--close');
+const burger = document.querySelector(".hamburger");
+const menuMobile = document.querySelector(".menu-mobile");
+const menuMobileLinks = document.querySelectorAll(".menu-mobile__item a");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("hamburger--close");
+  menuMobile.classList.toggle("menu-mobile--close");
 });
-for(menuMobileLink of menuMobileLinks){
-  menuMobileLink.addEventListener('click', () => {
-    burger.classList.toggle('hamburger--close');
-    menuMobile.classList.toggle('menu-mobile--close');
+for (menuMobileLink of menuMobileLinks) {
+  menuMobileLink.addEventListener("click", () => {
+    burger.classList.toggle("hamburger--close");
+    menuMobile.classList.toggle("menu-mobile--close");
+  });
+}
+
+//ACCORDEON
+
+let clients = document.querySelectorAll('.accordeon__client');
+let quotes = document.querySelectorAll('accordeon__quote');
+for(let client of clients){
+  client.addEventListener('click', () => {
+    client.classList.toggle('accordeon__client--active');
+    client.firstElementChild.classList.toggle('arrow--down');
+    client.firstElementChild.classList.toggle('arrow--up');
+    let quote = client.nextElementSibling;
+    if(quote.style.maxHeight){
+      quote.style.maxHeight = null;
+    } else {
+      quote.style.maxHeight = quote.scrollHeight + 'px';
+    }
   })
 }
