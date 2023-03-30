1e4d6cd19285869317fc78efac42023ec741c21e
P <paula.deville@posteo.net>
2023-03-30 18:19:29 +0200

    scroll triggers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const loader = document.querySelector(".loader");
 const navElements = document.querySelectorAll(".menu-desktop__item");
-const arrows = document.querySelectorAll("header p");
+const arrows = document.querySelectorAll("#welcome p");
 
 const introTimeline = gsap.timeline({
   defaults: {
@@ -26,6 +26,77 @@ introTimeline
     autoAlpha: 1,
     stagger: 0.2,
   });
+
+//SCROLL ANIMATIONS
+
+const aboutTL = gsap.timeline({
+  scrollTrigger: "#clients",
+});
+aboutTL
+  .to(".stack p", {
+    x: 20,
+    autoAlpha: 1,
+    ease: "power-4",
+    duration: 1,
+  })
+  .from(
+    ".mobile-portrait",
+    {
+      autoAlpha: 0,
+    },
+    "<"
+  );
+
+gsap.from(".accordeon__client p", {
+  scrollTrigger: {
+    trigger: "#work",
+  },
+  y: 50,
+  autoAlpha: 0,
+});
+
+for (let card of document.querySelectorAll(".desktop-card--right")) {
+  gsap.from(card, {
+    scrollTrigger: {
+      trigger: card,
+      start: "top 80%",
+      toggleActions: "play reset play restart",
+    },
+    translateX: 100,
+    duration: 1,
+    autoAlpha: 0,
+  });
+}
+for (let card of document.querySelectorAll(".desktop-card--left")) {
+  gsap.from(card, {
+    scrollTrigger: {
+      trigger: card,
+      start: "top 80%",
+      toggleActions: "play reset play restart",
+    },
+    translateX: -100,
+    duration: 1,
+    autoAlpha: 0,
+  });
+}
+
+gsap.from(".mobile-card:nth-child(1)", {
+  scrollTrigger: {
+    trigger: ".mobile-card:nth-child(1)",
+    scrub: true,
+    end: "bottom center",
+  },
+  autoAlpha: 0,
+});
+
+gsap.from("#wordpress", {
+  scrollTrigger: {
+    trigger: "#wordpress",
+    scrub: 1,
+  },
+  autoAlpha: 0,
+  duration: 1,
+});
 
 //HAMBURGER
 
